db746ebf43dd4ecb181f622e36d25767b667a448
Jay Vachhani <jay@jaivel.com>
2020-09-02 16:51:50 +0100

    Intial working blank slate of CRA, clean up uneccessary items

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+
+import './App.css';
+
+class App extends Component {
+	render() {
+    	return (
+    	    <>
+
+			</>
+		);
+	}
+}
+
+export default App;
