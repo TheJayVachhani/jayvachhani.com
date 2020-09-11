29ce7509fac952d9024b300c8226584008c559da
TheJayVachhani <jay.vachhani@outlook.com>
2020-09-11 09:53:47 +0100

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# jayvachhani.tech
-This is my personal website built with React, Material-UI
-Currently in Development,
-
-You can use npm install and then npm start to see it's current state.
---
+++ precache-manifest.174a6d7006f299c59e80bd6b86e5ffb3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d18772e118a5ee6bdc4948d008517e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70931d877c96561dab9f",
+    "url": "/static/css/main.3d006b5e.chunk.css"
+  },
+  {
+    "revision": "d21687703133b4295d0b",
+    "url": "/static/js/2.f8b929f4.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.f8b929f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70931d877c96561dab9f",
+    "url": "/static/js/main.a82b381c.chunk.js"
+  },
+  {
+    "revision": "d931143917d651f575bb",
+    "url": "/static/js/runtime-main.cc115f75.js"
+  },
+  {
+    "revision": "d34d901f6d4525bcf881b7391fa3eaaa",
+    "url": "/static/media/Bolivia-Abandoned-Railway.d34d901f.png"
+  },
+  {
+    "revision": "ec44c90e44dfa474ff08a3380477cb68",
+    "url": "/static/media/Bolivia-Amazon.ec44c90e.png"
+  },
+  {
+    "revision": "44d9ab7bbe9b0c8dcd8219ef70e52c30",
+    "url": "/static/media/Bolivia-MtTarija.44d9ab7b.png"
+  },
+  {
+    "revision": "5839b03047e908ac0ae4ed6be54e3764",
+    "url": "/static/media/Bolivia-Salt-Flats.5839b030.png"
+  },
+  {
+    "revision": "0c4a88d9fa16e1677ded2098e8879953",
+    "url": "/static/media/Speyer-Quiet-Street.0c4a88d9.png"
+  },
+  {
+    "revision": "aaaca069fec49e02f30d8c7493c24f97",
+    "url": "/static/media/Zermatt-Glacier.aaaca069.png"
+  },
+  {
+    "revision": "9ecb6ee31e934ac6d04cdb5bc6b54b5b",
+    "url": "/static/media/Zermatt-Lodge.9ecb6ee3.png"
+  }
+]);
